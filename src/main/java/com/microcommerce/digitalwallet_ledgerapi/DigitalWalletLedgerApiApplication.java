1de049d374186832ac13daad401cc645bc1eac52
VitoScalletta <muhammetemre3737@gmail.com>
2026-05-07 16:13:27 +0300

    Faz 1 : Proje Oluşturuldu

---
+++ src/main/java/com/microcommerce/digitalwallet_ledgerapi/DigitalWalletLedgerApiApplication.java
@@ -0,0 +1,13 @@
+package com.microcommerce.digitalwallet_ledgerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalWalletLedgerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalWalletLedgerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microcommerce/digitalwallet_ledgerapi/DigitalWalletLedgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcommerce.digitalwallet_ledgerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalWalletLedgerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
