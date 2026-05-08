0e5e25167ce82820abf5f7e27e02bbc28f3ce62d
VitoScalletta <muhammetemre3737@gmail.com>
2026-05-08 18:19:58 +0300

    Faz 1:User ekleme çalıştırıldı test edildi Başarılı

--- src/main/java/com/microcommerce/digitalwallet_ledgerapi/common/entity/BaseEntity.java
+++ src/main/java/com/microcommerce/digitalwallet_ledgerapi/common/entity/BaseEntity.java
@@ -8,11 +8,9 @@ import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDateTime;
 
-@EnableJpaAuditing
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
--- src/main/java/com/microcommerce/digitalwallet_ledgerapi/user/controller/UserController.java
+++ src/main/java/com/microcommerce/digitalwallet_ledgerapi/user/controller/UserController.java
@@ -1,11 +1,25 @@
 package com.microcommerce.digitalwallet_ledgerapi.user.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.microcommerce.digitalwallet_ledgerapi.user.dto.CreateUserRequest;
+import com.microcommerce.digitalwallet_ledgerapi.user.entity.User;
+import com.microcommerce.digitalwallet_ledgerapi.user.repository.UserRepository;
+import com.microcommerce.digitalwallet_ledgerapi.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+    private final UserRepository userRepository;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createUser(@Valid @RequestBody CreateUserRequest request) {
+        userService.createUser(request);
+    }
 }
